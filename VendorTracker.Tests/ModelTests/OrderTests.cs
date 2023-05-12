e417424d9e9180e373ace44a01419cd7e1946b53
Kyle Crawford <kcraw217@gmail.com>
2023-05-12 10:56:08 -0700

    add vendor class, constructor, and methods ClearAll, GetAll, Find, AddOrder

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,23 @@ namespace VendorTracker.Tests
       Order newOrder = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsName_String()
+    {
+      DateOnly orderDate = new DateOnly(2023, 5, 12);
+      Order newOrder = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate);
+      string expected = "Bread";
+      Assert.AreEqual(expected, newOrder.Title);
+    }
+
+    [TestMethod]
+    public void GetDate_ReturnsDate_DateOnly()
+    {
+      DateOnly orderDate = new DateOnly(2023, 5, 12);
+      Order newOrder = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate);
+      DateOnly expected = new DateOnly(2023, 5, 12);
+      Assert.AreEqual(expected, newOrder.Date);
+    }
   }
 }
--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -6,8 +6,75 @@ using System;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
-    
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Bob", "Deli Owner");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Bob";
+      Vendor newVendor = new Vendor(name, "Deli Owner");
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name, "Deli Owner");
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Bob";
+      string name02 = "Evan";
+      Vendor newVendor1 = new Vendor(name01, "Deli Owner");
+      Vendor newVendor2 = new Vendor(name02, "Deli Owner");
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "Bob";
+      string name02 = "Evan";
+      Vendor newVendor1 = new Vendor(name01, "Deli Owner");
+      Vendor newVendor2 = new Vendor(name02, "Deli Owner");
+      Vendor result = Vendor.Find(2);
+      Assert.AreEqual(newVendor2, result);
+    }
+
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      string title = "Bread";
+      string description = "Walk the dog.";
+      float price = 12.99F;
+      DateOnly date = new DateOnly(2023, 5, 11);
+      Order newOrder = new Order(title, description, price, date);
+      List<Order> newList = new List<Order> { newOrder };
+      string name = "Bob";
+      Vendor newVendor = new Vendor(name, "Deli Owner");
+      newVendor.AddOrder(newOrder);
+      List<Order> result = newVendor.Orders;
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
