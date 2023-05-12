d30ee4d9ee305017670858fb8de34ef796642ff4
Kyle Crawford <kcraw217@gmail.com>
2023-05-12 11:16:02 -0700

    add instances list, methods ClearAll, GetAll, and Find

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -1,13 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorTracker.Models;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
@@ -32,6 +37,37 @@ namespace VendorTracker.Tests
       Order newOrder = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate);
       DateOnly expected = new DateOnly(2023, 5, 12);
       Assert.AreEqual(expected, newOrder.Date);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      DateOnly orderDate1 = new DateOnly(2023, 5, 12);
+      DateOnly orderDate2 = new DateOnly(2023, 5, 3);
+      Order newOrder1 = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate1);
+      Order newOrder2 = new Order("Pastries", "4 muffins, 2 croissants", 22.40F, orderDate2);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      DateOnly orderDate1 = new DateOnly(2023, 5, 12);
+      DateOnly orderDate2 = new DateOnly(2023, 5, 3);
+      Order newOrder1 = new Order("Bread", "7 loaves of sourdough", 15.50F, orderDate1);
+      Order newOrder2 = new Order("Pastries", "4 muffins, 2 croissants", 22.40F, orderDate2);
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
     }
   }
 }
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace VendorTracker.Models
 {
   public class Order
   {
     public string Title { get; set; }
+
     public string Description { get; set; }
     public float Price { get; set; }
     public DateOnly Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
 
     public Order(string title, string description, float price, DateOnly date)
     {
@@ -15,6 +19,22 @@ namespace VendorTracker.Models
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
