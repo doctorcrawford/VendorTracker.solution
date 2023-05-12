c2c04d1bd104e66516a537e16a7432092f7712f8
Kyle Crawford <kcraw217@gmail.com>
2023-05-12 12:49:06 -0700

    add routes to add new orders to vendor

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -28,5 +28,30 @@ namespace VendorTracker.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorItems = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorItems);
+      return View(model);
+    }
+
+    // [HttpPost("/vendors/{vendorId}/orders")]
+    // public ActionResult Create(int vendorId, string orderTitle, string orderDescription, float orderPrice, DateTime orderDate)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor foundVendor = Vendor.Find(vendorId);
+    //   DateOnly orderDateOnly = DateOnly.FromDateTime(orderDate);
+    //   Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDateOnly);
+    //   foundVendor.AddOrder(newOrder);
+    //   List<Order> categoryOrders = foundVendor.Orders;
+    //   model.Add("vendor", foundVendor);
+    //   model.Add("orders", categoryOrders);
+    //   return View("Show", model);
+    // }
+
   }
 }
