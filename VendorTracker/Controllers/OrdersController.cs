912ea91afd5ee74d5d3b554852c660c51e623b75
Evan Johnson <evanrjohnso@gmail.com>
2023-05-19 10:08:11 -0700

    use controller level attribute. use file scoped namespace

--- VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker/Controllers/OrdersController.cs
@@ -3,28 +3,27 @@ using System;
 using System.Collections.Generic;
 using VendorTracker.Models;
 
-namespace VendorTracker.Controllers
+namespace VendorTracker.Controllers;
+
+[Route("vendors/{vendorId}/orders")]
+public class OrdersController : Controller
 {
-  public class OrdersController : Controller
+
+  [HttpGet("new")]
+  public ActionResult New(int vendorId)
   {
+    Vendor vendor = Vendor.Find(vendorId);
+    return View(vendor);
+  }
 
-    [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
-    {
-      Vendor vendor = Vendor.Find(vendorId);
-      return View(vendor);
-    }
-
-    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
-    public ActionResult Show(int vendorId, int orderId)
-    {
-      Order order = Order.Find(orderId);
-      Vendor vendor = Vendor.Find(vendorId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
-      return View(model);
-    }
-
+  [HttpGet("{orderId}")]
+  public ActionResult Show(int vendorId, int orderId)
+  {
+    Order order = Order.Find(orderId);
+    Vendor vendor = Vendor.Find(vendorId);
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    model.Add("order", order);
+    model.Add("vendor", vendor);
+    return View(model);
   }
 }
--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -3,61 +3,60 @@ using System;
 using System.Collections.Generic;
 using VendorTracker.Models;
 
-namespace VendorTracker.Controllers
+namespace VendorTracker.Controllers;
+
+[Route("vendors")]
+public class VendorsController : Controller
 {
-  public class VendorsController : Controller
+
+  [HttpGet("")]
+  public ActionResult Index()
   {
+    List<Vendor> allVendors = Vendor.GetAll();
+    return View(allVendors);
+  }
 
-    [HttpGet("/vendors")]
-    public ActionResult Index()
-    {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
-    }
+  [HttpGet("new")]
+  public ActionResult New()
+  {
+    return View();
+  }
 
-    [HttpGet("/vendors/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+  [HttpPost("")]
+  public ActionResult Create(string vendorName, string vendorDescription)
+  {
+    Vendor newVendor = new Vendor(vendorName, vendorDescription);
+    return RedirectToAction("Index");
+  }
 
-    [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName, string vendorDescription)
-    {
-      Vendor newVendor = new Vendor(vendorName, vendorDescription);
-      return RedirectToAction("Index");
-    }
+  [HttpGet("{id}")]
+  public ActionResult Show(int id)
+  {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Vendor selectedVendor = Vendor.Find(id);
+    List<Order> vendorItems = selectedVendor.Orders;
+    model.Add("vendor", selectedVendor);
+    model.Add("orders", vendorItems);
+    return View(model);
+  }
 
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorItems = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("orders", vendorItems);
-      return View(model);
-    }
+  [HttpPost("{vendorId}/orders")]
+  public ActionResult Create(int vendorId, string orderTitle, string orderDescription, float orderPrice, DateTime orderDate)
+  {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Vendor foundVendor = Vendor.Find(vendorId);
+    DateOnly orderDateOnly = DateOnly.FromDateTime(orderDate);
+    Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDateOnly);
+    foundVendor.AddOrder(newOrder);
+    List<Order> categoryOrders = foundVendor.Orders;
+    model.Add("vendor", foundVendor);
+    model.Add("orders", categoryOrders);
+    return View("Show", model);
+  }
 
-    [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, float orderPrice, DateTime orderDate)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundVendor = Vendor.Find(vendorId);
-      DateOnly orderDateOnly = DateOnly.FromDateTime(orderDate);
-      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDateOnly);
-      foundVendor.AddOrder(newOrder);
-      List<Order> categoryOrders = foundVendor.Orders;
-      model.Add("vendor", foundVendor);
-      model.Add("orders", categoryOrders);
-      return View("Show", model);
-    }
-
-    [HttpGet("/vendors/{vendorId}/orders")]
-    public ActionResult Show(Dictionary<string, object> model)
-    {
-      return View(model);
-    }
-    
+  [HttpGet("{vendorId}/orders")]
+  public ActionResult Show(Dictionary<string, object> model)
+  {
+    return View(model);
   }
 }
